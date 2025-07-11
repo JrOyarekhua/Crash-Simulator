8f077ca094abeb32a78ad50d4cde4047386aaf9a
Gabriel Jr Oyarekhua <jroyarekhua@Gabriels-MacBook-Air.local>
2025-07-10 23:36:48 -0400

    added shared crash settings logic

---
+++ Assets/Scripts/CrashSettings.cs
@@ -0,0 +1,7 @@
+public static class CrashSettings{
+    public static float CarMass;
+    public static float Car2Mass;
+    public static float InitialVelocity;
+    public static int SceneType = 0; 
+    public static float Elasticity;  
+}

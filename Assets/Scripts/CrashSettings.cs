b5b54a2f8ae0f0dddb229dffbeec12500547dcee
Gabriel Jr Oyarekhua <jroyarekhua@Gabriels-MacBook-Air.local>
2025-07-15 23:12:11 -0400

    added physics calculator class

--- Assets/Scripts/CrashSettings.cs
+++ Assets/Scripts/CrashSettings.cs
@@ -4,5 +4,7 @@ public static class CrashSettings{
     public static float CarOneInitialVelocity;
     public static float CarTwoInitialVelocity; 
     public static int SceneType = 0; 
-    public static float Elasticity;  
+    public static float Elasticity;
+    public static bool toggleUI = true;
+    public static bool hasSpawned = false;
 }
